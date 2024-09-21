ed505d701e50f794bcd9e4d7d82bdd0ec4cd76bf
LeQuy-123 <ledaosiquy@gmail.com>
2024-09-21 22:18:49 +0700

    add some popup delivery result

---
+++ Assets/Scripts/UI/DelveryResultUI.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DelveryResultUI : MonoBehaviour
+{
+    private string POP_UP_TRIGGER = "DeliveryResultPopup";
+    [SerializeField] private Image backgroundImage;
+    [SerializeField] private TextMeshProUGUI textMessage;
+    [SerializeField] private Image iconImage;
+    [SerializeField] private Color successColor;
+    [SerializeField] private Color failureColor;
+    [SerializeField] private Sprite successSprite;
+    [SerializeField] private Sprite failureSprite;
+    Animator animator;
+    private float displayTimer;
+    private float displayTimerMax;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        displayTimerMax = 3f;
+        displayTimer = displayTimerMax;
+    }
+    private void Start()
+    {
+        Hide();
+        DeliveryManager.Instance.OnRecipeSuccess+= DeliveryManager_OnRecipeSuccess;
+        DeliveryManager.Instance.OnRecipeFailure += DeliveryManager_OnRecipeFailure;
+    }
+    // private void Update()
+    // {
+    //     if(displayTimer > 0)
+    //     {
+    //         displayTimer -= Time.deltaTime;
+    //         if(displayTimer <= 0)
+    //         {
+    //             Hide();
+    //         }
+    //     } else {
+    //         displayTimer = displayTimerMax;
+    //     }
+    // }
+
+    private void DeliveryManager_OnRecipeFailure(object sender, EventArgs e)
+    {
+        animator.SetTrigger(POP_UP_TRIGGER);
+        textMessage.text = "DELIVERY\nFAILURE!";
+        backgroundImage.color = failureColor;
+        iconImage.sprite = failureSprite;
+        Show();
+    }
+
+    private void DeliveryManager_OnRecipeSuccess(object sender, EventArgs e)
+    {
+        animator.SetTrigger(POP_UP_TRIGGER);
+        textMessage.text = "DELIVERY\nSUCCESS!";
+        backgroundImage.color = successColor;
+        iconImage.sprite = successSprite;
+        Show();
+    }
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
